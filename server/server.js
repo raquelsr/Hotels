f2d829406ad9289621881337135cfff7f66b39ce
raquelsr <raquelserranor@hotmail.com>
2019-08-11 18:39:45 +0200

    accept CORSin server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ app.listen(3000, () => {
     console.log("Server running on port 3000...");
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/hotels", (req, res) => {
     res.json(data);
 });
