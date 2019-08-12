7eff4f875923468d316e33de08b90021be4d461f
raquelsr <raquelserranor@hotmail.com>
2019-08-12 17:39:44 +0200

    added and passed eslint

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const app = express();
-
+const express = require('express');
 const data = require('../data/hotels.json');
 
+const app = express();
+
 app.listen(3000, () => {
-    console.log("Server running on port 3000...");
+  console.log('Server running on port 3000...');
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-app.get("/hotels", (req, res) => {
-    res.json(data);
+app.get('/hotels', (req, res) => {
+  res.json(data);
 });
--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
 
 async function showHotels() {
+  const hotels = await this.getHotels();
+  const list = document.createElement('ul');
 
-    const hotels = await this.getHotels();
-    const list = document.createElement('ul');
+  // Con fragment se evita el cross-site
+  const fragment = document.createDocumentFragment();
 
-    // Con fragment se evita el cross-site
-    const fragment = document.createDocumentFragment();
+  hotels.forEach((hotel) => {
+    const li = document.createElement('li');
+    li.textContent = hotel.name;
+    fragment.appendChild(li);
+  });
 
-    hotels.forEach((hotel) => {
-        const li = document.createElement('li');
-        li.textContent = hotel.name;
-        fragment.appendChild(li);
-    });
+  list.appendChild(fragment);
 
-    list.appendChild(fragment);
-
-    const div = document.getElementsByClassName('hotels');
-    div[0].appendChild(list);
+  const div = document.getElementsByClassName('hotels');
+  div[0].appendChild(list);
 }
 
+// eslint-disable-next-line no-unused-vars
 async function getHotels() {
-    const response = await fetch('http://localhost:3000/hotels');
-    const data = await response.json();
-    return data.hotels;
+  const response = await fetch('http://localhost:3000/hotels');
+  const data = await response.json();
+  return data.hotels;
 }
 
 window.onload = showHotels;
-    
