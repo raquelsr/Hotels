fe76d1eb11881038d8edf9a51babd1610f5e65c5
raquelsr <raquelserranor@hotmail.com>
2019-09-29 23:53:25 +0200

    info, styles and css

--- src/main.js
+++ src/main.js
@@ -11,15 +11,20 @@ async function showHotels() {
   const fragment = document.createDocumentFragment();
 
   hotels.forEach((hotel) => {
-    const li = document.createElement('li');
-    li.textContent = hotel.name;
-    fragment.appendChild(li);
+    const node = createHotelNode(hotel);
+    fragment.appendChild(node);
   });
 
   list.appendChild(fragment);
 
   const div = document.getElementsByClassName('hotels');
   div[0].appendChild(list);
+
+  const accordions = document.getElementsByClassName('accordion');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < accordions.length; i++) {
+    controlClick(accordions[i]);
+  }
 }
 
 async function getHotels() {
@@ -28,4 +33,58 @@ async function getHotels() {
   return data.hotels;
 }
 
+function createHotelNode(hotel) {
+  const li = document.createElement('li');
+  li.classList.add('accordion');
+  const title = document.createElement('h2');
+  const divInfo = document.createElement('div');
+  divInfo.classList.add('panel');
+  const p = document.createElement('p');
+  p.textContent = hotel.price;
+  title.textContent = hotel.name;
+  divInfo.appendChild(p);
+  li.appendChild(title);
+  li.appendChild(divInfo);
+  addInfo(divInfo, hotel);
+  return li;
+}
+
+function controlClick(accordion) {
+  accordion.addEventListener('click', () => {
+    const panel = accordion.getElementsByClassName('panel');
+    toggleClass(panel[0]);
+  });
+}
+
+function toggleClass(panel) {
+  if (panel.style.display === 'none') {
+    panel.style.display = 'block';
+  } else {
+    panel.style.display = 'none';
+  }
+}
+
+function addInfo(divInfo, hotel) {  
+  const img = addImage(hotel);
+  const stars = addStarts(hotel.rating);
+  console.log(img)
+  divInfo.appendChild(img);
+  divInfo.appendChild(stars);
+}
+
+function addImage(hotel) {
+  const imgElement = document.createElement('img');
+  imgElement.setAttribute('src', `../${hotel.imgUrl}`);
+  imgElement.setAttribute('alt', hotel.name);
+  return imgElement;
+}
+
+function addStarts(stars) {
+  const p = document.createElement('p');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < stars; i++) {
+    p.textContent += String.fromCodePoint(0x2B50);
+  }
+  return p;
+}
 window.onload = showHotels;
